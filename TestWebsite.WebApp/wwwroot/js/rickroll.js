bb4e18aa06e7fb695fc0e902d70eccb29f7902df
Troll Coder <math323n@edu.campusvejle.dk>
2020-11-02 13:44:47 +0100

    added something

---
+++ TestWebsite.WebApp/wwwroot/js/rickroll.js
@@ -0,0 +1,12 @@
+﻿function getRickrolled(callback) {
+    var promise = new Promise((resolve) => {
+        setTimeout(() => resolve("http://www.youtube.com/watch?v=dQw4w9WgXcQ"), 1000)
+    })
+    promise.then(callback)
+    return promise
+}
+getRickrolled(function (rickroll) {
+    console.log("Callback: " + rickroll)
+}).then(function (rickroll) {
+    console.log("Promise: " + rickroll)
+})
